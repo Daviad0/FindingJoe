dd33e10603fc29be292c8210e36eddd62bbb690b
David Reeves <djreeve693@pccsk12.com>
2023-01-30 10:10:05 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,304 @@
+// setup expressjs server
+
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+var Datastore = require('nedb');
+
+var allowedPreviously = [];
+var currentState = {
+    room : "electrical",
+    sender: "",
+    votes: [],
+    uuid: ""
+}
+
+var lastLocked = {
+    by: "server",
+    end: new Date("2020-01-01T00:00:00.000Z")
+
+};
+
+function generateUUID(){
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
+
+
+dotenv.config();
+var db = {};
+db.trust = new Datastore({ filename: 'data/trust.db', autoload: true });
+db.banished = new Datastore({ filename: 'data/banished.db', autoload: true });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/main.html");
+});
+app.get("/info*", function(req, res) {
+    res.sendFile(__dirname + "/views/info.html");
+});
+
+
+function checkIfLocked(){
+    var now = new Date();
+    
+    return now < lastLocked.end;
+}
+
+
+function checkIfInRange(key){
+    
+    var n = 0;
+    try{
+        n = parseInt(key);
+    }catch(e){
+        return false;
+    }
+    
+
+    var ranges = process.env.ALLOWED_RANGES.split(",");
+    for(var i = 0; i < ranges.length; i++){
+        if(ranges[i].includes("-")){
+            var n1 = parseInt(ranges[i].split("-")[0]);
+            var n2 = parseInt(ranges[i].split("-")[1]);
+            if(n >= n1 && n <= n2){
+                return true;
+            }
+        }
+        if(n == parseInt(ranges[i])){
+            return true;
+        }
+    }
+    return false;
+}
+
+function attemptLogin(key, cb){
+
+    if (key == process.env.JOE_KEY) {
+        
+        cb("joe");
+        return;
+    }
+    if(key == process.env.ADMIN_KEY) {
+        cb("admin");
+        return;
+    }
+
+    db.banished.find({ key: key }, function(err, docs) {
+        if(docs.length > 0) {
+            
+            cb("");
+        }else{
+            
+            if(allowedPreviously.includes(key)){
+                cb("normal");
+            }else if(checkIfInRange(key)){
+                allowedPreviously.push(key);
+                cb("normal");
+            }else{
+                    
+                cb("");
+            }
+
+        }
+    });
+}
+
+app.post("/api/lock", (req, res) => {
+    var key = req.cookies.key;
+    attemptLogin(key, function(access){
+        if(access == ""){
+            res.status(401).send({ status: "error", error: "not_allowed" });
+            return;
+        }
+        if(access == "normal"){
+            res.status(401).send({ status: "error", error: "not_allowed" });
+            return;
+        }
+        if(checkIfLocked()){
+            res.status(400).send({ status: "error", error: "already_locked" });
+            return;
+        }
+
+        if(!checkIfLocked()){
+            var newDate = new Date();
+            if(access == "joe"){
+                newDate.setMinutes(newDate.getMinutes() + 10);
+                lastLocked.end = newDate;
+                lastLocked.by = "Joe";
+            }else{
+                newDate.setMinutes(newDate.getMinutes() + 60);
+                lastLocked.end = newDate;
+                lastLocked.by = "Administrator";
+            }
+        }
+        
+        
+        res.send({ status: "success" });
+    });
+});
+
+app.post("/api/unlock", (req, res) => {
+    var key = req.cookies.key;
+    attemptLogin(key, function(access){
+        if(access == ""){
+            res.status(401).send({ status: "error", error: "not_allowed" });
+            return;
+        }
+        if(access == "normal"){
+            res.status(401).send({ status: "error", error: "not_allowed" });
+            return;
+        }
+        if(!checkIfLocked()){
+            res.status(400).send({ status: "error", error: "not_locked" });
+            return;
+        }
+        lastLocked.end = new Date("2020-01-01T00:00:00.000Z");
+        res.send({ status: "success" });
+    });
+});
+
+app.post("/api/state", (req, res) => {
+    var authKey = req.cookies.key;
+    
+    attemptLogin(authKey, function(access){
+        if(access == ""){
+            res.status(401).send({ status: "error", error: "not_allowed" });
+            return;
+        }
+        if(checkIfLocked()){
+            res.status(400).send({ status: "error", error: "locked" });
+            return;
+        }
+        
+        var previousUpvotes = currentState.votes.filter(v => v.vote == "UPVOTE").length;
+        var previousDownvotes = currentState.votes.filter(v => v.vote == "DOWNVOTE").length;
+        var sender = currentState.sender;
+        db.trust.find({ key: sender }, function(err, docs) {
+            if(docs.length > 0){
+                docs[0].upvotes += previousUpvotes;
+                docs[0].downvotes += previousDownvotes;
+                db.trust.update({ key: sender }, docs[0], {}, function(err, numReplaced){
+
+                });
+            }else{
+                db.trust.insert({ key: sender, upvotes: previousUpvotes, downvotes: previousDownvotes }, function(err, newDoc){
+
+                });
+            }
+        });
+
+        var room = req.body.room;
+
+        if(authKey == process.env.ADMIN_KEY){
+            authKey = "admin";
+        }
+        if(authKey == process.env.JOE_KEY){
+            authKey = "joe";
+        }
+
+        currentState = {
+            room: room,
+            sender: authKey,
+            votes: [],
+            uuid: generateUUID()
+        };
+        res.send({ status: "success" });
+
+    });
+});
+
+
+app.post("/api/auth", (req, res) => {
+    var authKey = req.body.authKey;
+    
+    attemptLogin(authKey, function(access){
+        if(access == ""){
+            res.status(401).send({ status: "error", error: "not_allowed" });
+            return;
+        }
+        res.send({ status: "success", access: access });
+    });
+    
+
+})
+
+app.get("/api/currentState", (req, res) => {
+    var key = req.cookies.key;
+    attemptLogin(key, function(access){
+        if(access == ""){
+            res.status(401).send({ status: "error" });
+            return;
+        }
+    
+        var sendBackCurrentState = {
+            room: currentState.room,
+            sender: currentState.sender,
+            vote: currentState.votes.find(v => v.sender == key),
+            uuid: currentState.uuid
+        }
+    
+        res.send({ status: "success", currentState: (checkIfLocked() ? undefined : sendBackCurrentState), locked: (checkIfLocked() ? lastLocked : undefined) });
+    });
+
+    
+});
+
+app.post("/api/vote", (req, res) => {
+    var key = req.cookies.key;
+    attemptLogin(key, function(access){
+        if(access == ""){
+            res.status(401).send({ status: "error" });
+            return;
+        }
+    
+        // allow for UPVOTE or DOWNVOTE votes
+    
+        var vote = req.body.vote;
+        if(vote != "UPVOTE" && vote != "DOWNVOTE"){
+            res.status(400).send({ status: "error", error: "invalid vote" });
+            return;
+        }
+
+        var pseudoKey = key;
+        if(key == process.env.ADMIN_KEY){
+            pseudoKey = "admin";
+        } else if(key == process.env.JOE_KEY){
+            pseudoKey = "joe";
+        }
+        if(currentState.sender == pseudoKey){
+            res.status(400).send({ status: "error", error: "this is your post" });
+            return;
+        }
+
+        var prevVote = currentState.votes.find(v => v.sender == key);
+        if(prevVote != undefined){
+            currentState.votes.splice(currentState.votes.indexOf(prevVote), 1);
+            currentState.votes.push({ sender: pseudoKey, vote: vote });
+        }else{
+            currentState.votes.push({ sender: pseudoKey, vote: vote });
+        }
+        res.send({ status: "success" });
+    });
+
+    
+});
+
+app.listen(port, function() {
+    console.log("Server started @ localhost:" + port);
+});
---
+++ public/main.js
@@ -0,0 +1,247 @@
+document.addEventListener('DOMContentLoaded', async function() {
+    // read key cookie
+    var key = "";
+    try{
+        key = document.cookie.split('; ').find(row => row.startsWith('key=')).split('=')[1];
+    }catch(e){
+
+    }
+    if(key == ""){
+        switchToScreen(1);
+        return;
+    }
+    
+    var loginResult = await postData('/api/auth', {authKey: key});
+    if(loginResult.success && loginResult.data.status != "error"){
+        myKey = key;
+        switchToScreen(2);
+        document.querySelector("#loggedin").style.display = "";
+        document.querySelector("#authkey").innerHTML = key
+        document.getElementById("lock").style.display = "none";
+        if(loginResult.data.access == "admin"){
+            document.querySelector("#authkey").innerHTML = "Administrator"
+            document.getElementById("lock").style.display = "";
+            myKey = "admin";
+        }else if(loginResult.data.access == "joe"){
+            document.querySelector("#authkey").innerHTML = "Joe"
+            document.getElementById("lock").style.display = "";
+            myKey = "joe";
+        }
+        
+        
+        refreshState();
+    }else{
+        switchToScreen(1);
+    }
+});
+
+var myKey = "";
+
+// create a function to POST data to a server
+const postData = async ( url = '', data = {})=>{
+    const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        // Body data type must match "Content-Type" header        
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+
+    try {
+        const newData = await response.json();
+        return {success: true, data: newData};
+    }catch(error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+    return {success: false};
+}
+
+const getData = async ( url = '', data = {})=>{
+    const response = await fetch(url, {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
+
+    try {
+        const newData = await response.json();
+        return {success: true, data: newData};
+    }catch(error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+    return {success: false};
+}
+
+var currentState = undefined;
+
+async function logout(){
+    // clear cookie
+    document.cookie = "key=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    // refresh page
+    document.querySelector("#key").value = "";
+    document.querySelector("#loggedin").style.display = "none";
+    switchToScreen(1);
+}
+
+async function lock(){
+    if(lockedUntil != undefined){
+        var result = await postData('/api/unlock', {});
+        refreshState();
+    }else{
+        var result = await postData('/api/lock', {});
+        refreshState();
+    }
+    
+}
+
+async function submitRoom(){
+    const room = document.getElementById("roomselect").value;
+    if(room == ""){
+        alert("Please select a room");
+        return;
+    }
+    if(room == currentState.room){
+        return;
+    }
+
+    var result = await postData('/api/state', {room: room});
+    if(result.success){
+        refreshState();
+    }
+}
+
+var lockedUntil = undefined;
+
+async function refreshState(){
+    var result = await getData('/api/currentState');
+    if(result.success){
+        
+        currentState = result.data.currentState;
+
+        if(currentState == undefined){
+            // then the environment is locked
+            lockedUntil = new Date(result.data.locked.end);
+            document.getElementById("subtitle").innerHTML = "Locked by " + result.data.locked.by + " for...";
+            document.getElementById("upvote").style.display = "none";
+            document.getElementById("downvote").style.display = "none";
+            document.getElementById("submitnewstatus").style.display = "none";
+            document.getElementById("lock").style.color = "#ffbbbb";
+
+        }else{
+            
+            lockedUntil = undefined;
+            document.getElementById("subtitle").innerHTML = "Joe is currently in...";
+            document.getElementById("upvote").style.display = "";
+            document.getElementById("downvote").style.display = "";
+            document.getElementById("lock").style.color = "";
+            document.getElementById("submitnewstatus").style.display = "";
+            if(currentState.room == ""){
+                document.getElementById("room").innerText = "UNKNOWN";
+                document.querySelector("#upvote").style.color = "#cccccc";
+            }else{
+                document.getElementById("room").innerText = currentState.room.substring(0, 1).toUpperCase() + currentState.room.substring(1);
+                document.querySelector("#upvote").style.color = "";
+                
+                document.getElementById("roomselect").value = currentState.room;
+                if(currentState.vote != undefined){
+                    document.querySelector("#" + currentState.vote.vote.toLowerCase()).style.color = (currentState.vote.vote == "UPVOTE") ? "#bbffbb" : "#ffbbbb";
+                }
+                if(currentState.sender == myKey){
+                    document.querySelector("#upvote").style.color = "#cccccc";
+                    document.querySelector("#downvote").style.color = "#cccccc";
+    
+                }
+            }
+        }
+
+        
+
+        
+        
+    }
+}
+
+async function submitVote(vote){
+
+    if(currentState == undefined || currentState.room == "")
+        return;
+
+    document.querySelector("#upvote").style.color = "";
+    document.querySelector("#downvote").style.color = "";
+
+    var result = await postData('/api/vote', {vote: vote});
+    if(result.success && result.data.status != "error"){
+        document.querySelector("#" + vote.toLowerCase()).style.color = (vote == "UPVOTE") ? "#bbffbb" : "#ffbbbb";
+    }else{
+        document.querySelector("#" + vote.toLowerCase()).style.color = "#cccccc";
+        setTimeout(function(){
+            document.querySelector("#" + vote.toLowerCase()).style.color = "";
+        }, 1000);
+    }
+}
+
+async function tryLogin(){
+
+    const key = document.getElementById("key").value;
+    if(key == ""){
+        alert("Please enter a key");
+        return;
+    }
+
+    const data = {authKey: key};
+    var result = await postData('/api/auth', data);
+    if(result.success){
+        // save key to cookie
+        if(result.data.status == "error"){
+            document.querySelector("#login").style.color = "#ffbbbb";
+        }else{
+            myKey = key;
+            document.cookie = "key=" + key;
+            document.querySelector("#login").style.color = "";
+            document.querySelector("#loggedin").style.display = "";
+            document.querySelector("#authkey").innerHTML = key
+            document.getElementById("lock").style.display = "none";
+            if(result.data.access == "admin"){
+                myKey = "admin";
+                document.getElementById("lock").style.display = "";
+                document.querySelector("#authkey").innerHTML = "Administrator"
+            }else if(result.data.access == "joe"){
+                myKey = "joe";
+                document.getElementById("lock").style.display = "";
+                document.querySelector("#authkey").innerHTML = "Joe"
+            }
+            
+    
+            refreshState();
+            switchToScreen(2);
+        }
+        
+    }
+}
+
+function switchToScreen(screen){
+    const screens = document.getElementsByClassName("screen");
+    for(var i = 0; i < screens.length; i++){
+        screens[i].style.display = "none";
+    }
+    document.querySelector(".screen[data-id='" + screen +"']").style.display = "block";
+}
+
+setInterval(refreshState, 5000);
+setInterval(function(){
+    if(lockedUntil != undefined){
+        // UPDATE LOCK UNTIL;
+        var diff = lockedUntil - new Date();
+        var seconds = Math.floor(diff / 1000);
+        var minutes = Math.floor(seconds / 60);
+        var showSeconds = (seconds % 60).toString().padStart(2, "0");
+
+        document.getElementById("room").innerText = minutes + ":" + showSeconds; 
+    }
+}, 200);
